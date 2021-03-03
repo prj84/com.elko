a028cbd7c9f858b200c33b9cef9375cbb1ff3e51
Pål Raymond Johansen <35060010+prj84@users.noreply.github.com>
2021-03-03 13:53:56 +0100

    Added support for ELKO Smartplug

---
+++ drivers/SmartPlug/device.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const ELKOSMARTGenericDevice = require('../../lib/ELKOSMARTGenericDevice');
+
+class SmartPlug extends ELKOSMARTGenericDevice {
+
+  ELKOSMARTEndpoint = 1;
+
+}
+module.exports = SmartPlug;
