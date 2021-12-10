66d1de0847cea11f20fd53634f7326c562c89ad2
Pål Hefre <prj_84@hotmail.com>
2021-12-10 15:03:34 +0100

    Fixing error handling to prevent device unavailable on a few devices

--- drivers/ESH316GLED/device.js
+++ drivers/ESH316GLED/device.js
@@ -12,40 +12,45 @@ class ESH316GLED extends ZigBeeLightDevice {
       this.setAvailable();
 
       if (this.hasCapability('onoff')) {
-        this.registerCapability('onoff', CLUSTER.ON_OFF);
-        this.getClusterCapabilityValue('onoff', CLUSTER.ON_OFF);
+        try {
+          this.registerCapability('onoff', CLUSTER.ON_OFF);
+          this.getClusterCapabilityValue('onoff', CLUSTER.ON_OFF);
+
+          await this.configureAttributeReporting([
+            {
+                endpointID: 1,
+                cluster: CLUSTER.ON_OFF,
+                attributeName: 'onOff',
+                minInterval: 0,
+                maxInterval: 300,
+                minChange: 0,
+            },
+          ]);
+        } catch (err) {
+          this.error('Error in registering or getting capability value (on/off): ', err)
       }
+      }
+
       if (this.hasCapability('dim')) {
-        this.registerCapability('dim', CLUSTER.LEVEL_CONTROL);
-        this.getClusterCapabilityValue('dim', CLUSTER.LEVEL_CONTROL);
+        try {
+          this.registerCapability('dim', CLUSTER.LEVEL_CONTROL);
+          this.getClusterCapabilityValue('dim', CLUSTER.LEVEL_CONTROL);
 
-
-await this.configureAttributeReporting([
-  {
-      endpointID: 1,
-      cluster: CLUSTER.ON_OFF,
-      attributeName: 'onOff',
-      minInterval: 0,
-      maxInterval: 300,
-      minChange: 0,
-  },
-]);
-
-await this.configureAttributeReporting([
-  {
-      endpointID: 1,
-      cluster: CLUSTER.LEVEL_CONTROL,
-      attributeName: 'currentLevel',
-      minInterval: 0,
-      maxInterval: 300,
-      minChange: 0,
-  },
-]);
-
-
-
-  }
- }
+          await this.configureAttributeReporting([
+            {
+                endpointID: 1,
+                cluster: CLUSTER.LEVEL_CONTROL,
+                attributeName: 'currentLevel',
+                minInterval: 0,
+                maxInterval: 300,
+                minChange: 0,
+            },
+          ]);
+        } catch (err) {
+          this.error('Error in registering or getting capability value (dim): ', err)
+        }
+      }
+    }
 }
 
 
