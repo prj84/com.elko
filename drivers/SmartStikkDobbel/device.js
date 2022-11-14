60dd1b0511ff9201fd010c4703e345d78b7ed7a4
Pål Hefre <prj_84@hotmail.com>
2022-11-14 13:23:18 +0100

    Fixed kWh bug, return to old logic. Added SmartStikkDobbel & SmartStikkEnkel Added all Multiwire dimmer id's to code Made preperations for PIR devices

---
+++ drivers/SmartStikkDobbel/device.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const ELKOSMARTGenericDevice = require('../../lib/ELKOSMARTGenericDevice');
+
+class SmartStikkDobbel extends ELKOSMARTGenericDevice {
+
+  ELKOSMARTEndpoint = 1;
+
+}
+module.exports = SmartStikkDobbel;
